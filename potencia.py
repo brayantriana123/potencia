3c33b596b32e3436e0f56383e4381fc982df6281
brayantriana123 <brayantriana@colegioguanenta.edu.co>
2022-05-18 07:09:49 -0500

    Desarrollo: programa terminado en python. brayantriana

---
+++ potencia.py
@@ -0,0 +1,17 @@
+"""programa No. 2:
+programa para calcular x a la y"""
+
+print("--------------------------------")
+print("----potencia x a la y ----------")
+print("--------------------------------")
+
+# input
+x = int(input("Digite el valor de x: "))
+y = int(input("Digite el valor de y: "))
+
+# processing
+z = x**y
+
+# output
+print("\nResultados:\n")
+print(str(x) + " a la " + str(y) + " es igual a" + str(z))
